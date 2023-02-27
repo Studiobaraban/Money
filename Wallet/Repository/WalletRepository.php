bc14d32e73665654998c2cc485b26b42fa4ddee8
Animatik <studiobaraban@gmail.com>
2023-02-27 20:59:15 +0800

    Wallet repository

---
+++ Wallet/Repository/WalletRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\Wallet\Repository;
+
+use app\Wallet\Entity\Wallet;
+
+class WalletRepository extends \yii\db\ActiveRecord
+{
+
+    public function list($user_id, bool $asArray = false): ?array
+    {
+        $q = Wallet::find()->where(['user_id' => $user_id]);
+
+        if (!empty($asArray)) {
+            $q->asArray();
+        }
+
+        return $q->all();
+    }
+
+
+    public function one(int $id): ?Wallet
+    {
+        return Wallet::find()->where(['id' => $id])->one();
+    }
+
+}
