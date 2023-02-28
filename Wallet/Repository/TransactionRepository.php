516efbcd6cdde8e88f937bdee125ea8e2e902758
Animatik <studiobaraban@gmail.com>
2023-02-28 08:13:44 +0800

    HotFix transaction

--- Wallet/Repository/TransactionRepository.php
+++ Wallet/Repository/TransactionRepository.php
@@ -8,7 +8,7 @@ use app\Wallet\Entity\Transaction;
 class TransactionRepository
 {
 
-    public static function list(int $wallet_id, bool $asArray = false): ?array
+    public function list(int $wallet_id, bool $asArray = false): ?array
     {
         $q = Transaction::find()->where(['wallet_id' => $wallet_id, 'status' => 1]);
 
